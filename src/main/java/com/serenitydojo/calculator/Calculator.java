9a30a567cad6b4b282f34280286ab5b48243eec5
ACXGREZ <71022776+ACXGREZ@users.noreply.github.com>
2023-05-24 22:07:15 -0400

    Challenge 2: String Calculator

--- src/main/java/com/serenitydojo/calculator/Calculator.java
+++ src/main/java/com/serenitydojo/calculator/Calculator.java
@@ -1,7 +1,34 @@
-package com.serenitydojo.calculator;
-
-public class Calculator {
+package com.serenitydojo.calculator;public class Calculator {
     public int evaluate(String expression) {
-        return 0;
+        if (expression.isEmpty()) {
+            return 0;
+        }
+
+        String[] tokens = expression.split("\\s+");
+        int result = Integer.parseInt(tokens[0]);
+
+        for (int i = 1; i < tokens.length; i += 2) {
+            String operator = tokens[i];
+            int operand = Integer.parseInt(tokens[i + 1]);
+
+            switch (operator) {
+                case "+":
+                    result += operand;
+                    break;
+                case "-":
+                    result -= operand;
+                    break;
+                case "*":
+                    result *= operand;
+                    break;
+                case "/":
+                    result /= operand;
+                    break;
+                default:
+                    throw new IllegalMathsOperatorException("Unsupported operator: " + operator);
+            }
+        }
+
+        return result;
     }
 }
